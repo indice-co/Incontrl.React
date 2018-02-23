dad4495e65c806ad1bacc340de8d2499a51e2abb
Yiannis Tsenes <itsenes@indice.gr>
2018-02-23 14:03:32 +0200

    root element fallback

--- src/index.js
+++ src/index.js
@@ -4,31 +4,30 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-window.InContrl = function(id) {
-  var root = document.getElementById(id);
-  var token = root.getAttribute("token");
-  var subscriptionid = root.getAttribute("subscription-id");
-  var doctypeid = root.getAttribute("doc-type-id");
-  var docid = root.getAttribute("doc-id");
-  var view = root.getAttribute("view");
-  var environment = root.getAttribute("environment");
-  var debug = root.getAttribute("debug");
-  var culture = root.getAttribute("culture");
-  var userlink = root.getAttribute("user-link");
+var root = document.getElementById("root");
+var token = root.getAttribute("token");
+var subscriptionid = root.getAttribute("subscription-id");
+var doctypeid = root.getAttribute("doc-type-id");
+var docid = root.getAttribute("doc-id");
+var view = root.getAttribute("view");
+var environment = root.getAttribute("environment");
+var debug = root.getAttribute("debug");
+var culture = root.getAttribute("culture");
+var userlink = root.getAttribute("user-link");
 
-  ReactDOM.render(
-    <App
-      culture={culture}
-      debug={debug}
-      environment={environment}
-      token={token}
-      subscriptionid={subscriptionid}
-      doctypeid={doctypeid}
-      docid={docid}
-      view={view}
-      userlink={userlink}
-    />,
-    root
-  );
-};
+ReactDOM.render(
+  <App
+    culture={culture}
+    debug={debug}
+    environment={environment}
+    token={token}
+    subscriptionid={subscriptionid}
+    doctypeid={doctypeid}
+    docid={docid}
+    view={view}
+    userlink={userlink}
+  />,
+  root
+);
+
 registerServiceWorker();
